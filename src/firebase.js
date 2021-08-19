b8f3e5c47a5dbef6ee33f1a08410fde2867980c6
Haram Park <hayami226@naver.com>
2021-08-19 21:48:36 +0900

    Added Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABQAupcsz8969L_-XiLYOYHTV_p2uOWt8",
+    authDomain: "nwitter-4579a.firebaseapp.com",
+    projectId: "nwitter-4579a",
+    storageBucket: "nwitter-4579a.appspot.com",
+    messagingSenderId: "149182410201",
+    appId: "1:149182410201:web:4c286a8111be938252fdc8"
+};
+
+export default firebase.initializeApp(firebaseConfig);
