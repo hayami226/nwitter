8d46e22df554bf5c1da51c1f9553f7288bd03fd1
Haram Park <hayami226@naver.com>
2021-09-07 21:24:39 +0900

    Added Snapshot

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     // 로그인 되어있는지 확인
     authService.onAuthStateChanged((user) => {
       if(user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -18,7 +20,7 @@ function App() {
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initialazing..."}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initialazing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
