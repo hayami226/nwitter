1b9ff68fae44af31be9780f7222a79aa8c63b521
Haram Park <hayami226@naver.com>
2021-09-08 21:37:00 +0900

    Added Delete and Edit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 
@@ -37,9 +38,7 @@ const Home = ({ userObj }) => {
             </form>
             <div>
                 {nweets.map(nweet => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet key={nweet.id} nweetObj={nweet} isOwner={userObj.uid == nweet.creatorId} />
                 ))}
             </div>
         </div>
